c631ddd7301b1934aa0f1a166eaa413d62bbe054
YanC Lin <yonalin1@gmail.com>
2018-03-23 21:57:35 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20180323211331) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
